39f81785cc1e2cb16a80d98afde3168b4507828a
Andreas Blaette <andreas.blaette@uni-due.de>
2017-01-30 09:10:52 +0100

    rework similarity

--- R/similarity_method.R
+++ R/similarity_method.R
@@ -1,24 +1,25 @@
+setGeneric("cosine_similarity", function(.Object ) standardGeneric("cosine_similarity"))
+
 #' Calculate cosine Similarity
 #' 
 #' Calculate cosine similarity of two vectors.
-#' Note: For pairwise comparisons of vectors, this is much faster 
-#' than proxy::simil
+#' Note: Much faster for pairwise comparisons than proxy::simil.
 #' @param i integer index, the n-th position of the simple triplet matrix Y
 #' @param X matrix from which the values are taken
 #' @param Y simple triplet matrix
 #' @param ... further parameters (for use with blapply)
-cosine_similarity <- function(i, X, Y, ...){
-  a <- as.vector(X[,Y$i[i]])
-  b <- as.vector(X[,Y$j[i]])
+setMethod("cosine_similarity", "numeric", function(.Object, X, Y, ...){
+  a <- as.vector(X[,Y$i[.Object]])
+  b <- as.vector(X[,Y$j[.Object]])
   cProd <- crossprod(a) * crossprod(b)
   cProdSqrt <- sqrt(cProd)
   crossprod(a, b) / cProdSqrt
-}
+})
 
 
 
 
-#' Similarity Calculations for Matrices
+#' Similarity Calculations for Matrices.
 #'
 #' @param .Object a simple_triplet_matrix
 #' @param select a simple_triplet_matrix
@@ -29,40 +30,71 @@ cosine_similarity <- function(i, X, Y, ...){
 #' @export similarity
 #' @importFrom slam simple_triplet_matrix
 #' @importFrom proxy as.simil
-similarity <- function(.Object, chunks = 1, select = NULL, method = "cosine", return.simil = TRUE, progress = TRUE, verbose = TRUE, mc = FALSE){
+setMethod("cosine_similarity", "simple_triplet_matrix", function(.Object, chunks = 1, select = NULL, tool = "simil", method = "cosine", return.simil = TRUE, progress = TRUE, verbose = TRUE, mc = FALSE){
   stopifnot(is(.Object)[1] %in% c("TermDocumentMatrix", "simple_triplet_matrix", "Matrix"))
-  if (chunks == 1){
-    if (is.null(select)){
-      combinations <- utils::combn(1:ncol(.Object), 2)
-      select <- slam::simple_triplet_matrix(
-        i = combinations[1,],
-        j = combinations[2,],
-        v = rep(FALSE, times = ncol(combinations)),
-        dimnames = list(colnames(.Object), colnames(.Object))
-        )
-    }
-    if (verbose) message("... calculating similarities")
-    
-    cosineValues <- blapply(
-      c(1:length(select$i)),
-      f = cosine_similarity,
-      X = .Object, Y = select,
-      mc = mc, progress = progress
+  if (is.null(select)){
+    combinations <- utils::combn(1:ncol(.Object), 2)
+    select <- slam::simple_triplet_matrix(
+      i = combinations[1,],
+      j = combinations[2,],
+      v = rep(FALSE, times = ncol(combinations)),
+      dimnames = list(colnames(.Object), colnames(.Object))
     )
-    
-    if (verbose) message("... preparing matrix to be returned")
-    select$v <- unlist(cosineValues)
-    if (return.simil == TRUE){
-      return(proxy::as.simil(as.matrix(select)))  
-    } else {
-      return(select)
-    }
-  } else if (chunks > 1){
-    M <- blapply(
-      .Object, f = proxy::simil,
-      method = method, chunks = chunks,
-      progress = progress, mc = mc, verbose = FALSE
-      )
-    return(proxy::as.simil(M))
   }
-}
+  if (verbose) message("... calculating similarities")
+  
+  cosineValues <- blapply(
+    c(1:length(select$i)),
+    f = cosine_similarity,
+    X = .Object, Y = select,
+    mc = mc, progress = progress
+  )
+  
+  if (verbose) message("... preparing matrix to be returned")
+  select$v <- unlist(cosineValues)
+  if (return.simil == TRUE){
+    return(proxy::as.simil(as.matrix(select)))  
+  } else {
+    return(select)
+  }
+})
+
+setMethod("cosine_similarity", "matrix", function(.Object, tool = "proxy", chunks = 1, progress = TRUE, verbose = TRUE, mc = FALSE){
+  if (tool == "proxy"){
+    if (chunks > 1){
+      M <- blapply(
+        .Object, f = proxy::simil,
+        method = "cosine", chunks = chunks,
+        progress = progress, mc = mc, verbose = FALSE
+      )
+    } else {
+      M <- proxy::simil(.Object, method = "cosine", by_rows = TRUE)
+    }
+  } else if (tool == ""){
+    
+  } else if (tool == "coop"){
+    M <- coop::cosine(t(.Object))
+  } else if (tool == "handmade"){
+    cp <- crossprod(t(.Object))
+    rtdg <- sqrt(diag(.Object))
+    foo <- tcrossprod(rtdg)
+    M <- cp / foo
+  }
+  proxy::as.simil(M)
+})
+
+setMethod("cosine_similarity", "big.matrix", function(.Object){
+  M_transposed <- as.big.matrix(t(.Object))
+  cp <- .Object %*% M_transposed
+  diag <- sapply(1:ncol(cp), function(i) cp[i,i])
+  rtdg <- sqrt(diag)
+  tcross <- as.big.matrix(matrix(rtdg, ncol = 1)) %*% as.big.matrix(matrix(rtdg, nrow = 1))
+  pbapply::pblapply(
+    1:nrow(cp),
+    function(i) cp[i,] <- cp[i,] / tcross[i,] 
+  )
+  options(bigmemory.allow.dimnames = TRUE)
+  rownames(cp) <- rownames(vec)
+  colnames(cp) <- rownames(vec)
+  cp
+})
